017c6cf3239a253f6a743496c535fa7e6d78192f
Michał Szozdowski <49624645+MSzozdowski@users.noreply.github.com>
2022-11-06 15:19:10 +0100

    add usb projects

---
+++ T9_L14_USBHostMSC/Core/Src/main.c
@@ -0,0 +1,279 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "fatfs.h"
+#include "usart.h"
+#include "usb_host.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+extern ApplicationTypeDef Appli_state;
+
+extern uint8_t retUSBH;    /* Return value for USBH */
+extern char USBHPath[4];   /* USBH logical drive path */
+extern FATFS USBHFatFS;    /* File system object for USBH logical drive */
+extern FIL USBHFile;       /* File object for USBH */
+
+uint8_t Bytes;
+char Data[128];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void MX_USB_HOST_Process(void);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USB_HOST_Init();
+  MX_FATFS_Init();
+  /* USER CODE BEGIN 2 */
+  uint8_t OneTime = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  switch(Appli_state) {
+	  case APPLICATION_IDLE:
+
+		  break;
+	  case APPLICATION_START:
+		  HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin, SET);
+		  retUSBH = f_mount(&USBHFatFS, (const TCHAR*)USBHPath, 0);
+		  if(retUSBH != FR_OK)
+		  {
+		  	  Bytes = sprintf(Data, "FatFS mount error.\n\r");
+		  	  HAL_UART_Transmit(&huart2, (uint8_t*)Data, Bytes, 1000);
+		  }
+		  else
+		  {
+			  Bytes = sprintf(Data, "FatFS mounted.\n\r");
+			  HAL_UART_Transmit(&huart2, (uint8_t*)Data, Bytes, 1000);
+		  }
+		  break;
+	  case APPLICATION_READY:
+		  if((OneTime == 0) && (retUSBH == FR_OK))
+		  {
+			  //
+			  // Open file on pendrive for writing
+			  //
+			  retUSBH = f_open(&USBHFile, "test.txt", FA_WRITE|FA_CREATE_ALWAYS);
+			  //
+			  // File open error check
+			  //
+			  if(retUSBH != FR_OK)
+			  {
+				  Bytes = sprintf(Data, "No test.txt file. Can't create.\n\r");
+				  HAL_UART_Transmit(&huart2, (uint8_t*)Data, Bytes, 1000);
+			  }
+			  else
+			  {
+				  Bytes = sprintf(Data, "File opened.\n\r");
+				  HAL_UART_Transmit(&huart2, (uint8_t*)Data, Bytes, 1000);
+				  //
+				  //	Print something to this file
+				  //
+				  for(uint8_t i = 0; i < 10; i++)
+				  {
+					  f_printf(&USBHFile, "Line number %d.\n", i);
+				  }
+
+				  //
+				  // Close file
+				  //
+				  retUSBH = f_close(&USBHFile);
+
+				  Bytes = sprintf(Data, "File closed.\n\r");
+				  HAL_UART_Transmit(&huart2, (uint8_t*)Data, Bytes, 1000);
+
+				  retUSBH = f_open(&USBHFile, "test.txt", FA_READ);
+				  if(retUSBH != FR_OK)
+				  {
+					  Bytes = sprintf(Data, "No test.txt file. Can't open \n\r");
+					  HAL_UART_Transmit(&huart2, (uint8_t*)Data, Bytes, 1000);
+				  }
+				  else
+				  {
+					  UINT len;
+					  do
+					  {
+						  len = 0;
+						  f_read(&USBHFile, Data, 10, &len);
+						  HAL_UART_Transmit(&huart2, (uint8_t*)Data, len, 1000);
+					  }while(len >0);
+					  retUSBH = f_close(&USBHFile);
+				  }
+			  }
+			  OneTime = 1;
+		  }
+		  break;
+	  case APPLICATION_DISCONNECT:
+		  HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin, RESET);
+		  OneTime = 0;
+		  break;
+	  default:
+		  break;
+	  }
+
+    /* USER CODE END WHILE */
+    MX_USB_HOST_Process();
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
